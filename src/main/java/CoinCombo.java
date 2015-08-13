570a8d136ff097d9dd9da1cca54bdd0e11e91e32
perry eising & Teresa <student@epicodus.com>
2015-08-13 11:36:55 -0700

    while loop and if statement working, adding more later.

--- src/main/java/CoinCombo.java
+++ src/main/java/CoinCombo.java
@@ -19,18 +19,41 @@ public class CoinCombo {
       Map<String, Object> model = new HashMap<String, Object>();
       model.put("template", "templates/output.vtl");
 
-      //get the cent amount from the form
+      //get the cent amount from the form and change it to an integer
       String stringUserInput = request.queryParams("userInput");
       Integer intUserInput = Integer.parseInt(stringUserInput);
 
       //output the change to the page
-      model.put("intUserInput", intUserInput);
+      String outPut = createChange(intUserInput);
+      model.put("outPut", outPut);
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
   }
 
-  // public static createChange () {
-  //
-  // }
+   public static String createChange (Integer intUserInput) {
+
+  Integer quarters = 0;
+  Integer dimes = 0;
+  Integer nickels = 0;
+  Integer pennies = 0;
+
+  while ( intUserInput > 0 )
+  {
+
+    if ( intUserInput >= 25)
+    {
+      intUserInput -= 25;
+      quarters = quarters+=1;
+    }
+    else{ //code doesn't know how to do this yet!!
+      break;
+    }
+
+  }
+
+String  returnText= "You have used: " + quarters;
+
+  return returnText;
+  }
 
 }
